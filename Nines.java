ac3ac2aeacf29995ae33166673219840cb5763a7
htbrdd <htbrdd@gmail.com>
2014-12-28 18:26:44 -0500

    nines

---
+++ Nines.java
@@ -0,0 +1,12 @@
+package java24book;
+
+public class Nines {
+	public static void main(String[] arguments){
+		for (int dex = 1; dex <= 200; dex++) {
+			int multiple = 9 * dex;
+			System.out.print(multiple + " ");
+			
+		}
+	}
+
+}
